141e44e955852e181450d22b6fe65e6a4ea096ad
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-03-29 03:08:27 +0500

    Add structure in index.html,
    Add structure in index.html, Add to do list dynamically using index.js and add styles in style.css

--- dist/runtime.bundle.js
+++ dist/runtime.bundle.js
@@ -182,7 +182,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkwebpack_installation"] = self["webpackChunkwebpack_installation"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkto_do_list"] = self["webpackChunkto_do_list"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const tasks = [
+  { description: 'Learn HTML', completed: false, index: 1 },
+  { description: 'Learn CSS', completed: false, index: 2 },
+  { description: 'Learn JavaScript', completed: false, index: 3 },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const todoList = document.getElementById('todo-list');
 
-  return element;
-}
+const renderTasks = () => {
+  tasks.forEach((task) => {
+    const taskCard = document.createElement('div');
+    taskCard.classList = 'task-content';
+    taskCard.innerHTML = `<div class="task-text">
+                            <input type="checkbox">
+                            <p class="task-text">${task.description}</p>
+                          </div>
+                          <i class="fa-solid fa-trash-can" id="delete-task"></i>`;
+    todoList.appendChild(taskCard);
+  });
+};
 
-document.body.appendChild(component());
+renderTasks();
